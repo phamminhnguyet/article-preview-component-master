b1ffe8e3ac4863e3be3a649d83ac7577a31d70e8
Apple <apple@Apples-MacBook-Pro-3.local>
2021-06-27 00:19:33 +0700

    modified button in responsive

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,14 @@
 function toggleSocialLinks() {
     var button = document.getElementById("button-toggle");
     var socialLinks = document.getElementById("social-links");
-    if (socialLinks.style.display === "none") {
+    if (socialLinks.style.visibility === "hidden") {
         button.classList.remove("button-inactive");
         button.classList.add("button-active");
-        socialLinks.style.display = "block";
+        socialLinks.style.visibility = "visible";
     } else {
         button.classList.remove("button-active");
         button.classList.add("button-inactive");
-        socialLinks.style.display = "none";
+        socialLinks.style.visibility = "hidden";
     }
 }
 
